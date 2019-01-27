d822d2026696e7310e18b0c8617f51971c91a3e7
bassiebangura <devbassiebangura@gmail.com>
2019-01-27 11:36:17 -0500

    Add comments to index.js file.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 $('#search__box').keyup(function(){
     let value = $(this).val().toLowerCase();
      if(value == ""){
+         /*selects and shows all images if search box is empty*/
          $('.image_gallery').show();
      } else {
          $('img').each(function(){
              let caption = $(this).attr("alt").toLowerCase();
+             /*shows parent div based on img alt attribute value in comparism to value of search box*/
              (caption.indexOf(value) >= 0) ? $(this).parents('a').parents('.image_gallery').show() : $(this).parents('a').parents('.image_gallery').hide();
          });
     };
